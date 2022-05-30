ca0b33a10a26654a26f1e6f885aff07f37f14e97
Rafat Hussain <rafat.hsn@gmail.com>
2022-05-30 17:45:29 +0530

    print route

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,27 @@ pub fn min_dist(dist: Vec<u32>,visited: Vec<bool>) -> u32 {
     mindex
 }
 
+pub fn collect_route(route: Vec<i32>, l: usize) {
+    if route[l] != -1 {
+        collect_route(route.clone(),route[l as usize].clone().try_into().unwrap());
+        println!("{} ",l);
+    }
+}
+
 pub fn shortest_path(graph: Vec<u32>,N: u32,s: u32 ) -> Vec<u32> {
     let mut visited = Vec::<bool>::new();
     let mut dist = Vec::<u32>::new();
+    let mut route = Vec::<i32>::new();
 
-    for i in 0..N {
+    for _i in 0..N {
         visited.push(false);
         dist.push(u32::MAX);
+        route.push(-1);
     }
 
     dist[s as usize] = 0;
 
-    for i in 0..N-1 {
+    for _i in 0..N-1 {
         let u: u32 = min_dist(dist.clone(),visited.clone());
         visited[u as usize] = true;
 
@@ -33,8 +42,15 @@ pub fn shortest_path(graph: Vec<u32>,N: u32,s: u32 ) -> Vec<u32> {
             let ind = u * N + v;
             if !visited[v as usize] && graph[ind as usize] != 0 && dist[u as usize] != u32::MAX && (dist[u as usize] + graph[ind as usize] < dist[v as usize]) {
                 dist[v as usize] = dist[u as usize] + graph[ind as usize];
+                route[v as usize] = u as i32;
             }
         }
+
+    }
+
+    for i in 1..N {
+        println!("{} -> {} Route {}", s, i, s);
+        collect_route(route.clone(), i as usize);
     }
 
     dist
@@ -46,44 +62,44 @@ fn main() {
     let graph: Vec<u32> = vec![0, 4, 0, 0, 0, 0, 0, 8, 0,4, 0, 8, 0, 0, 0, 0, 11, 0,0, 8, 0, 7, 0, 4, 0, 0, 2, 0, 0, 7, 0, 9, 14, 0, 0, 0,
     0, 0, 0, 9, 0, 10, 0, 0, 0, 0, 0, 4, 14, 10, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1, 6, 8, 11, 0, 0, 0, 0, 1, 0, 7, 0, 0, 2, 0, 0, 0, 6, 7, 0];
 
-    println!("{:?}", graph);
+    //println!("{:?}", graph);
 
-    println!("SHORTEST PATH MATRIX");
+    //println!("SHORTEST PATH MATRIX");
 
     let mut dist: Vec<u32> = shortest_path(graph.clone(),N.clone(),0);
 
-    println!("{:?}", dist);
+    //println!("{:?}", dist);
 
     dist = shortest_path(graph.clone(),N.clone(),1);
 
-    println!("{:?}", dist);
+    //println!("{:?}", dist);
 
     dist = shortest_path(graph.clone(),N.clone(),2);
 
-    println!("{:?}", dist);
+    //println!("{:?}", dist);
 
     dist = shortest_path(graph.clone(),N.clone(),3);
 
-    println!("{:?}", dist);
+    //println!("{:?}", dist);
 
     dist = shortest_path(graph.clone(),N.clone(),4);
 
-    println!("{:?}", dist);
+    //println!("{:?}", dist);
 
     dist = shortest_path(graph.clone(),N.clone(),5);
 
-    println!("{:?}", dist);
+    //println!("{:?}", dist);
 
     dist = shortest_path(graph.clone(),N.clone(),6);
 
-    println!("{:?}", dist);
+    //println!("{:?}", dist);
 
     dist = shortest_path(graph.clone(),N.clone(),7);
 
-    println!("{:?}", dist);
+    //println!("{:?}", dist);
 
     dist = shortest_path(graph.clone(),N.clone(),8);
 
-    println!("{:?}", dist);
+    //println!("{:?}", dist);
 
 }
